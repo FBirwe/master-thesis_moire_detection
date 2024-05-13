1d6e1eb7cd65b1a89f5b0becc4d4fbd1a4c5263c
FBirwe <frederic.birwe@web.de>
2024-05-13 20:00:37 +0200

    update calc results

--- 60_gebastel/Model/calc_model_results.py
+++ 60_gebastel/Model/calc_model_results.py
@@ -9,9 +9,29 @@ from torchvision import models, transforms
 from file_interaction import download_blob, upload_buffer
 from io import BytesIO
 import pandas as pd
+import json
+from datetime import datetime
+from pytorch_model_tools import img_to_fft, get_radius_map
+
 
 dotenv = load_dotenv()
 BATCH_SIZE = 64
+
+
+def load_experiment( experiment_name ):
+    json_buffer = download_blob( f'train_logs/{ experiment_name }.json' )
+    data = json.loads( json_buffer.getvalue() )
+    data['start_timestamp'] = datetime.fromisoformat(data['start_timestamp'])
+
+    if 'end_timestamp' in data:
+        data['end_timestamp'] = datetime.fromisoformat(data['end_timestamp'])
+
+    for t in data['tracked_metrics']:
+        t['timestamp'] = datetime.fromisoformat(t['timestamp'])
+        t['value'] = float(t['value'])
+    
+    return data
+
 
 def load_model( model_name ):
     bytesStream = download_blob( f'models/{ model_name }.pth' )
@@ -57,7 +77,7 @@ def load_dataset( dataset_name, datasets=None ):
     return tile_paths
 
 
-def cal_model_results( tile_paths, model ):
+def cal_model_results( tile_paths, model, data_type="spatial" ):
     # Datenset wird geladen
     current_batch = []
     results = []
@@ -71,12 +91,19 @@ def cal_model_results( tile_paths, model ):
             pred = model(batch)
             return pred.cpu().numpy().tolist()
 
+    if data_type == 'spatial':
+        transform = transforms.ToTensor()
+    else:
+        radius_map = get_radius_map()
+        transform = transforms.Compose([
+            transforms.Lambda(lambda img: img_to_fft(img, radius_map)),
+            transforms.ToTensor()
+        ])
 
     for tile_path in tqdm(tile_paths):
         with Image.open(tile_path) as tile:
-            transform = transforms.Compose([transforms.PILToTensor()])
-
-        current_batch.append(transform(tile) / 255)
+            current_batch.append(transform(tile))
+    
         if len(current_batch) == BATCH_SIZE:
             results += process_batch(torch.stack(current_batch))
             current_batch = []
@@ -88,6 +115,7 @@ def cal_model_results( tile_paths, model ):
 def main():
     model_name = sys.argv[1]
     dataset_name = sys.argv[2]
+    experiment_data = load_experiment( model_name )
     
     if len(sys.argv) < 4:
         datasets = None
@@ -101,7 +129,7 @@ def main():
     tile_paths = load_dataset( dataset_name, datasets=datasets )
     print("dataset loaded")
 
-    results = cal_model_results( tile_paths, model )
+    results = cal_model_results( tile_paths, model, data_type=experiment_data['hyper_parameters']['data_type'] )
     print("results calculated")
 
     df = convert_results_to_df( tile_paths, results )
